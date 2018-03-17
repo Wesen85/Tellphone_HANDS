6f19ecd06621c4169bff495a659d269647d10a52
Wesen85 <37442147+Wesen85@users.noreply.github.com>
2018-03-17 17:51:28 +0300

    Test task

---
+++ handstest.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="windows-1251">
+  <title>Test</title>
+  <meta name="description" content="Test">
+  <meta name="author" content="Wesen">
+  <link rel="stylesheet" href="ht_styles.css">  
+</head>
+<body>
+<? require("handstest_logic.php")?>
+<form method="POST" >
+   <input name="phone" placeholder="Укажите номер телефона" class="textbox" type="text" required />
+   <input name="submit" class="button" type="submit" value="Отправить" />
+</form>
+<div class="phoneOut" >
+<div>Строка поиска:</br> <i>	<?=$out?></i></div>
+	
+<div class="resultFB" ><?=$res?></div>
+<div class="resultFB" ><?=$resV?></div>
+ <div style="clear: left"></div>
+	</div>
+</body>
+</html>
---
+++ handstest_logic.php
@@ -0,0 +1,77 @@
+<?php
+//// Функции
+
+function VerifyPhone($phone) 
+{		
+	return $phone=trim(htmlspecialchars($phone,ENT_QUOTES));	
+}
+
+//// Функция с регулярным выражением
+function ParsePhone($phone)
+{
+	$tr=preg_match_all("#[0-9\s-\]\[()+]{7,19}#si",$phone,$phone_array);
+	if ($tr==0) return $res=array("В строке нет телефонов");
+foreach($phone_array[0] as $value)
+{
+	$temp=str_replace("+7","8",$value);
+	$temp=trim(str_replace(array(" ","+","-","(",")","[","]"),"",$temp));
+	$strlen=strlen($temp);
+	if($strlen < 7) $temp=null;
+	elseif($strlen==7) $temp="8495".$temp;
+	elseif ($strlen==10) $temp="8".$temp;
+	elseif ($strlen==11 && $temp{0}==7) $temp{0}="8";
+	elseif ($strlen==11 && $temp{0}!=8) $temp=null;
+	elseif ($strlen > 11 && $temp{0}==8) $temp=substr($temp,0,11);
+	else  $temp=null;
+	
+	if($temp) $res[]=$temp;
+}
+	return $res;
+}
+
+//// Функция методом перебора
+function ParsePhoneV($phone)
+{
+$strlen=iconv_strlen($phone);
+$temp="";$resAr=null;$res=null;
+for ($i = 0; $i < $strlen; $i++) {
+   if (is_numeric($phone{$i})) $temp.=$phone{$i};
+   elseif (!in_array( $phone{$i},array(" ","+","-","(",")","[","]")) && $temp ) {$resAr[]=$temp;$temp=null;}
+}
+if($temp) $resAr[]=$temp;
+if(!$resAr) return $res=array("В строке нет телефонов");
+foreach ($resAr as $value)
+{
+	$strlen=strlen($value);
+	if($strlen < 7) $value=null;
+	elseif($strlen==7) $value="8495".$value;
+	elseif ($strlen==10) $value="8".$value;
+	elseif ($strlen==11 && $value{0}==7) $value{0}="8";
+	elseif ($strlen==11 && $value{0}!=8) $value=null;
+	elseif ($strlen > 11 && $value{0}==8) $value=substr($value,0,11);
+	elseif ($strlen > 11 && $value{0}==7) $value="8".substr($value,1,10);
+	else $value=null;
+	if($value) $res[]=$value;
+}
+	return $res;
+}
+/////
+$res="Введите строку для проверки";
+$resV="";
+if($_POST["phone"]) 
+{
+	$phone=VerifyPhone($_POST["phone"]);
+	$out=$phone;
+
+$start = microtime(true); 
+	$resAr=ParsePhone($phone);
+$res= 'Время выполнения функции с регулярным выражением (ParsePhone) :  '.(microtime(true) - $start).' сек.</br>';		
+	if($resAr) $res.="Результат: </br>". implode("</br>",$resAr );	
+
+$start = microtime(true); 
+	$resArV=ParsePhoneV($phone);
+$resV=  'Время выполнения функции методом перебора (ParsePhoneV): '.(microtime(true) - $start).' сек.</br>';		
+if($resArV) $resV.="Результат: </br>". implode("</br>",$resArV);
+
+}
+?>
